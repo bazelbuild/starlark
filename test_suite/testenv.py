79c370ccf5c032531c41293dfb227bcf1614549a
Marwan Tammam <marwan.01010100@gmail.com>
2019-06-12 01:42:18 +0200

    Fix bugs in testenv and shell script for bazelci

--- test_suite/testenv.py
+++ test_suite/testenv.py
@@ -1,6 +1,6 @@
 STARLARK_BINARY_PATH = {
     "java": "external/io_bazel/src/main/java/com/google/devtools/starlark/Starlark",
-    "go": "external/net_starlark_go/cmd/starlark/darwin_amd64_stripped/starlark",
+    "go": "external/net_starlark_go/cmd/starlark/*/starlark",
     "rust": "external/starlark-rust/target/debug/starlark-repl",
 }
 STARLARK_TESTDATA_PATH = "test_suite/"
