6d8730aad6971cc3bbb19c9d254318934154e3c9
Jon Brandvein <brandjon@google.com>
2025-03-06 13:51:15 -0500

    Regen spec.md TOC as per the generator script
    Also update the generator script's test to fail with a helpful diff.

--- spec_md_gen_test.py
+++ spec_md_gen_test.py
@@ -1,5 +1,13 @@
+import sys
+from difflib import unified_diff
 import spec_md_gen_lib
 
-expected = spec_md_gen_lib.gen_spec_md()
-actual = open("spec.md", mode="r", encoding="utf-8").read()
-assert expected == actual
+if __name__ == "__main__":
+    actual = open("spec.md", mode="r", encoding="utf-8").read().split("\n")
+    expected = spec_md_gen_lib.gen_spec_md().split("\n")
+    if expected != actual:
+        print("Failed!\n")
+        diff = unified_diff(actual, expected, fromfile='spec.md', tofile='<generated>', n=10)
+        for line in diff:
+            print(line)
+        sys.exit(1)
